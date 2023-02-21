e5d55cbf1bcb669f410e17009239d4e00c67abc9
Lin <lgamboa@csub.edu>
2023-02-21 09:55:02 -0800

    Add files via upload
    contains cool down function

---
+++ lgamboa.cpp
@@ -0,0 +1,14 @@
+//Linda Gamboa
+
+void cool_down(float *color){
+    color[1] += 0.01;
+    color[2] += 0.01;
+
+    if(color[1] > 1.0){
+        color[1] = 0.1;
+    }
+    if(color[2] > 1.0){
+        color[2] = 0.1;
+    }
+    return;
+}
