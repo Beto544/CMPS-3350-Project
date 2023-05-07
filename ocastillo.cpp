fc1e9610598b40bef4c77b412c2d77e80f6b263a
Oscar <90999876+Shad0wMag1c@users.noreply.github.com>
2023-05-06 22:08:14 -0700

    updated code still working on collision

--- ocastillo.cpp
+++ ocastillo.cpp
@@ -3,6 +3,22 @@
 #include <ctime>
 #include "global.h"
 extern Bullet *b;
+extern bool boxHit;
+
+class Terrain {
+public:
+    float w, h;
+    float pos[10];
+    float radius;
+    unsigned char color[3];
+    void set_color(unsigned char col[3]) {
+        memcpy(color, col, sizeof(unsigned char) * 3);
+    }
+    Terrain();
+    Terrain(int wid, int hgt, int x, int y);
+} terrain[3];
+
+float tx, ty;
 
 void renderHill(float x)
 {
@@ -30,7 +46,6 @@ void renderHill(float x)
     glPopMatrix();
     glColor3f(1.0f, 1.0f, 1.0f);
 }
-
 void renderLand(float x) 
 {
     g.hill.color[0] = 150.0/255;
@@ -42,13 +57,94 @@ void renderLand(float x)
     glScalef(1.0f, 1.0f, 1.0f);
     glRotatef(g.hill.angle, 0.0f, 0.0f, 0.0f);
     glBegin(GL_QUADS);
-    glVertex2f(-10.0f, -100.0f);
+    glVertex2f(10.0f, -100.0f);
     glVertex2f(-10.0f, 100.0f);
     glVertex2f(10.0f, 100.0f);
     glVertex2f(10.0f, -100.0f);
     glEnd();
     glPopMatrix();
     glColor3f(1.0f, 1.0f, 1.0f);
+}
+
+Terrain::Terrain() 
+{
+    w = 0.0f;
+    h = 0.0f;
+    pos[0] = (gl.xres/2) - 200;
+    pos[1] = (gl.yres/4) - 55;
+    radius = 50;
+}
+
+Terrain::Terrain(int wid, int hgt, int x, int y) 
+{
+    w = wid;
+    h = hgt;
+    pos[0] = x;
+    pos[1] = y;
+}
+
+void renderTerrain()
+{   
+    for (int i = 0; i < 3; i++) {
+        for (int j = 300; j < 800; j+= 200) {
+            glPushMatrix();
+            glColor3ubv(terrain[i].color);
+            glTranslatef(terrain[i].pos[j], terrain[i].pos[j + 100], 0.0f);
+            glBegin(GL_POLYGON);
+            glVertex2f(-terrain[i].w, -terrain[i].h);
+            glVertex2f(-terrain[i].w, -terrain[i].h);
+            glVertex2f(terrain[i].w, -terrain[i].h);
+            glVertex2f(terrain[i].w, -terrain[i].h);
+            glVertex2f(terrain[i].w, -terrain[i].h);
+            glVertex2f(terrain[i].w, -terrain[i].h);
+            glVertex2f(terrain[i].w, -terrain[i].h);
+            glVertex2f(terrain[i].w, terrain[i].h);
+            glVertex2f(-terrain[i].w, -terrain[i].h);
+            glVertex2f(-terrain[i].w, -terrain[i].h);
+            glEnd();
+            glPopMatrix();
+            j++;
+        }
+    }
+}
+
+void checkCollison(Bullet *b, int i)
+{
+    for (int j = 0; j < 3; j++) {
+        if (b->pos[0] > terrain[j].pos[0] - terrain[j].w / 2 - 4.0f &&
+                b->pos[0] < terrain[j].pos[0] + terrain[j].w / 2 + 4.0f &&
+                b->pos[1] > terrain[j].pos[1] - terrain[j].h / 2 - 4.0f &&
+                b->pos[1] < terrain[j].pos[1] + terrain[j].h / 2 + 4.0f) {
+            boxHit = true;
+            tx = b->pos[0];
+            ty = b->pos[1];
+            memcpy(&g.barr[i], &g.barr[g.nbullets - 1],
+                    sizeof(Bullet));
+            g.nbullets--;
+            // damageBox(b);
+        }
+    }
+}
+void drawHills()
+{
+    for (float j = 325; j < 800; j+=200) {
+        renderHill(j);
+        renderLand(j);
+    }
+}
+void renderMissile()
+{
+    for (int i = 0; i < g.nbullets; i++) {
+        Bullet *b = &g.barr[i];
+        // Log("draw bullet...\n");
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.8, 0.8, 0.8);
+        glVertex2f(b->pos[0] - 10.0f, b->pos[1] + 5.0f);
+        glVertex2f(b->pos[0] - 10.0f, b->pos[1] - 5.0f);
+        glVertex2f(b->pos[0] + 10.0f, b->pos[1]);
+        glEnd();
+    }
 }
 
 void green_ship(float *color) {
