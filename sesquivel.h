94e6d6ab42e5cc063773e5c6efd16e94ace26b64
Stephanie E <123580665+sesquivel15@users.noreply.github.com>
2023-05-09 10:08:51 +0000

    Add files via upload

--- sesquivel.h
+++ sesquivel.h
@@ -4,6 +4,9 @@
 #include "fonts.h"
 #include "global.h"
 
+extern int changeMap(int background);
+extern void displayMap(int b_num);
+extern void getMap(GLuint day, GLuint night);
 extern void renderBars(int player, double stat);
 extern void renderHealthBar(float cannonVelocity, int player);
 extern void renderCannonPower(float cannonVelocity, int player);
